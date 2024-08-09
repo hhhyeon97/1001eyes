52c76f7783f23c396270f338fafb9e3cb06e23f8
minsol <kth083030@naver.com>
2024-08-09 18:40:50 +0900

    Bean 수동 등록 테스트

--- src/main/java/com/demo/myshop/MyshopApplication.java
+++ src/main/java/com/demo/myshop/MyshopApplication.java
@@ -2,12 +2,13 @@ package com.demo.myshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class) // Spring Security 인증 기능 제외
 public class MyshopApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MyshopApplication.class, args);
 	}
-
 }
+
+
