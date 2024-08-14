c969b8d5d7f56d4dbfd0a2b8ad3f03a3e2e6e024
minsol <kth083030@naver.com>
2024-08-14 19:25:48 +0900

    로그아웃 로그 추가 테스트

--- src/main/java/com/demo/myshop/security/CustomLogoutSuccessHandler.java
+++ src/main/java/com/demo/myshop/security/CustomLogoutSuccessHandler.java
@@ -24,10 +24,18 @@ public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
 
     @Override
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
-        if (authentication != null && authentication.getPrincipal() instanceof UserDetailsImpl) {
-            String username = ((UserDetailsImpl) authentication.getPrincipal()).getUsername();
-            jwtUtilWithRedis.invalidateUserTokens(username, response);
-            logger.info("레디스에서 토큰 제거 !!!", username);
+        if (authentication == null) {
+            logger.warn("Authentication object is null during logout.");
+        } else {
+            logger.info("Authentication Principal Type 체크 !!! : {}", authentication.getPrincipal().getClass().getName());
+            if (authentication.getPrincipal() instanceof UserDetailsImpl) {
+                String username = ((UserDetailsImpl) authentication.getPrincipal()).getUsername();
+                logger.info("로그아웃 시도 유저 이름: {}", username);
+                jwtUtilWithRedis.invalidateUserTokens(username, response);
+                logger.info("레디스에서 토큰 제거 !!!");
+            } else {
+                logger.warn("Unexpected principal type: {}", authentication.getPrincipal().getClass().getName());
+            }
         }
         logger.info("로그아웃 성공");
         response.setStatus(HttpServletResponse.SC_OK);
