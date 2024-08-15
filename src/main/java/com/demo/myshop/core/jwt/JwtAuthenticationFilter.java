1be85dde5ceb90a771ee9230fc80e15d62c7e15b
minsol <kth083030@naver.com>
2024-08-15 15:50:03 +0900

    로그인/아웃 응답 메세지 추가

--- src/main/java/com/demo/myshop/core/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/demo/myshop/core/jwt/JwtAuthenticationFilter.java
@@ -52,11 +52,27 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
         String token = jwtUtil.createToken(username, role);
         jwtUtil.addJwtToCookie(token, response);
+
+        // 응답 본문에 로그인 성공 메시지 추가
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        // 응답 메시지 작성
+        String responseMessage = "{\"message\":\"로그인 성공\"}";
+        response.getWriter().write(responseMessage);
     }
 
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
         log.info("로그인 실패");
-        response.setStatus(401);
+
+        // 실패 메시지 작성
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401 상태 코드
+
+        // 응답 메시지 작성
+        String responseMessage = "{\"message\":\"로그인 실패: 아이디 또는 비밀번호가 올바르지 않습니다.\"}";
+        response.getWriter().write(responseMessage);
     }
 }
