e6e0310738bacfb2d25361a87fea563f9eb0cba8
minsol <kth083030@naver.com>
2024-08-17 19:59:40 +0900

    receivers 정보 암호화/jwt토큰만료시로그아웃/유효성검증라이브러리추가

--- src/main/java/com/demo/myshop/dto/RegisterRequestDto.java
+++ src/main/java/com/demo/myshop/dto/RegisterRequestDto.java
@@ -1,5 +1,6 @@
 package com.demo.myshop.dto;
 
+import jakarta.validation.constraints.Email;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,9 +8,13 @@ import lombok.Setter;
 @Getter
 @Setter
 public class RegisterRequestDto {
+
     private String username;
     private String password;
+
+    @Email(message = "유효한 이메일 주소를 입력해 주세요.")
     private String email;
+
     private String phone;
     private String name; // 가입시에는 기본적으로 가입하는 사람 이름으로 받는 분 성함 컬럼에도 저장할 거임 !! (receiverName)
     private boolean admin = false;
