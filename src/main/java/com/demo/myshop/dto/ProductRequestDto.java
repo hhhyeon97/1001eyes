b8df949f6f281a1a4ff94c4fb237c3a997a1c306
minsol <kth083030@naver.com>
2024-08-18 04:24:42 +0900

    상품 관련 코드 리팩토링

--- src/main/java/com/demo/myshop/dto/ProductRequestDto.java
+++ src/main/java/com/demo/myshop/dto/ProductRequestDto.java
@@ -1,15 +1,22 @@
 package com.demo.myshop.dto;
 
+import com.demo.myshop.model.Product;
+import jakarta.validation.Valid;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class ProductRequestDto {
+
     private String title;
     private String description;
     private String category;
     private Integer price;
     private Integer stock;
     private String imageUrl;
+
+    public Product toProduct() {
+        return new Product(title, description, category, price, stock, imageUrl);
+    }
 }
