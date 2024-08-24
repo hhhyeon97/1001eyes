6205fa189cdc4620a95c80047dc0cd24c8cf0b9c
minsol <kth083030@naver.com>
2024-08-25 01:25:40 +0900

    도커컴포즈-api게이트웨이제외확인됨/유레카대시보드에뜨는건세모

--- order-service/src/main/java/com/demo/orderservice/repository/CartItemRepository.java
+++ order-service/src/main/java/com/demo/orderservice/repository/CartItemRepository.java
@@ -2,6 +2,8 @@ package com.demo.orderservice.repository;
 
 import com.demo.orderservice.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 }
