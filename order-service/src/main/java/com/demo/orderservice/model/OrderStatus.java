5433d9f4534cb8f6120b29715e438b633b513563
minsol <kth083030@naver.com>
2024-08-26 07:22:56 +0900

    오더 상태 추가

--- order-service/src/main/java/com/demo/orderservice/model/OrderStatus.java
+++ order-service/src/main/java/com/demo/orderservice/model/OrderStatus.java
@@ -2,6 +2,12 @@ package com.demo.orderservice.model;
 
 public enum OrderStatus {
     PENDING, // 주문 대기
+
+    // todo : 추가 상태
+    PAYING, // 결제 대기
+    COMPLETED,  // 결제 성공 = 주문 최종 완료
+    PREPARING, // 배송 준비중
+
     SHIPPED, // 배송중
     DELIVERED, // 배송 완료
     CANCELED, // 취소됨
