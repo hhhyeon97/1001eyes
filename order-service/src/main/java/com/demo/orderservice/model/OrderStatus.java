ff93e0ab1931e58aee6fa6c08f460299a9920558
minsol <kth083030@naver.com>
2024-08-28 02:03:00 +0900

    주문취소로직수정:즉시재고복구->레디스에저장했다가텀두고복구

--- order-service/src/main/java/com/demo/orderservice/model/OrderStatus.java
+++ order-service/src/main/java/com/demo/orderservice/model/OrderStatus.java
@@ -5,6 +5,7 @@ public enum OrderStatus {
     PAYING,            // 결제 대기 (결제 진행 중)
     TIME_OUT,          // 타임아웃 (주문 또는 결제 이탈 시)
     COMPLETED,         // 결제 성공 (주문 최종 완료)
+    // ========================================
     PREPARING,         // 배송 준비 중 (결제 완료 후)
     SHIPPED,           // 배송 중
     DELIVERED,         // 배송 완료
