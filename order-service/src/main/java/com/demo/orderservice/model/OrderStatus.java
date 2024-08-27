5cfb5642b5b14af6941b15d8382bde23753d1f59
minsol <kth083030@naver.com>
2024-08-27 21:39:27 +0900

    주문로직변경준비

--- order-service/src/main/java/com/demo/orderservice/model/OrderStatus.java
+++ order-service/src/main/java/com/demo/orderservice/model/OrderStatus.java
@@ -1,16 +1,14 @@
 package com.demo.orderservice.model;
 
 public enum OrderStatus {
-    PENDING, // 주문 대기
-
-    // todo : 추가 상태
-    PAYING, // 결제 대기
-    COMPLETED,  // 결제 성공 = 주문 최종 완료
-    PREPARING, // 배송 준비중
-
-    SHIPPED, // 배송중
-    DELIVERED, // 배송 완료
-    CANCELED, // 취소됨
-    RETURN_REQUESTED, // 반품 요청됨
-    RETURNED // 반품 완료
+    PENDING,           // 주문 대기 (주문 생성됨)
+    PAYING,            // 결제 대기 (결제 진행 중)
+    TIME_OUT,          // 타임아웃 (주문 또는 결제 이탈 시)
+    COMPLETED,         // 결제 성공 (주문 최종 완료)
+    PREPARING,         // 배송 준비 중 (결제 완료 후)
+    SHIPPED,           // 배송 중
+    DELIVERED,         // 배송 완료
+    CANCELED,          // 주문 취소됨
+    RETURN_REQUESTED,  // 반품 요청됨
+    RETURNED           // 반품 완료
 }
