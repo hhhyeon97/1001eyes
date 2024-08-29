dd78f91416b1c143229c6c1d6e5c99891aa9d416
minsol <kth083030@naver.com>
2024-08-29 18:55:31 +0900

    주문진입/결제진입api개선5-레디스ttl타임아웃된임시주문데이터제거및재고복구

--- order-service/src/main/java/com/demo/orderservice/service/OrderTimeoutScheduler.java
+++ order-service/src/main/java/com/demo/orderservice/service/OrderTimeoutScheduler.java
@@ -2,6 +2,7 @@ package com.demo.orderservice.service;
 
 
 import com.demo.orderservice.dto.PrepareOrderDto;
+import com.demo.orderservice.dto.PrepareOrderRequestDto;
 import com.demo.orderservice.model.OrderStatus;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +10,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
@@ -26,7 +28,7 @@ public class OrderTimeoutScheduler {
         this.slackNotifierService = slackNotifierService;
     }
 
-    @Scheduled(fixedRate = 60000)  // 1분마다 실행
+    @Scheduled(fixedRate = 3 * 60000)  // test : 3분마다 실행
     public void checkForTimeoutOrders() {
         log.info("Checking for timeout orders...");
 
@@ -60,13 +62,33 @@ public class OrderTimeoutScheduler {
 
     // 주문 상태를 TIME_OUT으로 업데이트하는 메서드
     private void updateOrderStatusToTimeout(String orderKey, PrepareOrderDto orderDto) {
-        // 상태를 TIME_OUT으로 변경
+        // 1. 상태를 TIME_OUT으로 변경
         orderDto.setStatus(OrderStatus.TIME_OUT);
         redisTemplate.opsForHash().put("orders", orderKey, orderDto);
         log.info("Order key {} has been set to TIME_OUT", orderKey);
 
-        // Slack 알림 발송
-        slackNotifierService.sendNotification("Order with key " + orderKey + " has timed out.");
+        // 2. 재고 복구
+        restoreStock(orderDto);
+
+        // 3. Redis에서 타임아웃된 주문 데이터 삭제
+        redisTemplate.opsForHash().delete("orders", orderKey);
+        log.info("Order key {} has been deleted from Redis", orderKey);
+
+        // 4. Slack 알림 발송
+        slackNotifierService.sendNotification("Order with key " + orderKey + " has timed out and been removed.");
+    }
+
+    private void restoreStock(PrepareOrderDto orderDto) {
+        List<PrepareOrderRequestDto> orderItems = orderDto.getOrderItems();
+        for (PrepareOrderRequestDto item : orderItems) {
+            Long productId = item.getProductId();
+            Integer quantity = item.getQuantity();
+            String stockKey = "stock:" + productId;
+
+            // 레디스에서 재고 복구
+            redisTemplate.opsForValue().increment(stockKey, quantity);
+            log.info("Stock for product {} has been restored by {}", productId, quantity);
+        }
     }
 
 }
